229f5b9e27967f0342b764d8f5d15ff0efa36a5d
Enguerran <engcolson@gmail.com>
2016-06-05 08:29:43 +0200

    setup eslint

--- src/Foo.js
+++ src/Foo.js
@@ -11,7 +11,7 @@ export default class Foo extends React.Component {
   render() {
     return (
       <div className="foo" />
-    )
+    );
   }
 }
 
--- test/Foo_test.js
+++ test/Foo_test.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { expect } from 'chai';
-import { shallow, mount, render } from 'enzyme';
-import Foo from '../src/Foo';
+import React from "react";
+import { expect } from "chai";
+import { shallow, mount } from "enzyme";
+import Foo from "../src/Foo";
 
 describe("A suite", function() {
   it("contains spec with an expectation", function() {
@@ -9,10 +9,10 @@ describe("A suite", function() {
   });
 
   it("contains spec with an expectation", function() {
-    expect(shallow(<Foo />).is('.foo')).to.equal(true);
+    expect(shallow(<Foo />).is(".foo")).to.equal(true);
   });
 
   it("contains spec with an expectation", function() {
-    expect(mount(<Foo />).find('.foo').length).to.equal(1);
+    expect(mount(<Foo />).find(".foo").length).to.equal(1);
   });
 });
